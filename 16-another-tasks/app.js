e7416916872bda90bdc696608cbcbfb6704919a6
ne0nden <gorlov.vladimir2008@gmail.com>
2024-07-13 15:55:32 +0300

    1-commit

---
+++ 16-another-tasks/app.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const taskManager = {
+    toDoList: [
+        {
+            title: 'Помыть посуду',
+            description: 'Вечером я должен помыть посуду',
+            id: 1,
+            priority: 1,
+        }
+    ],
+    addTask: function (title, priority, description) {
+        let nextId = 1;
+        if (this.toDoList.length > 0) {
+            let maxId = 0;
+            for (let task of this.toDoList) {
+                if (task.id > maxId) {
+                    maxId = task.id;
+                }
+            }
+            nextId = maxId + 1;
+        };
+
+        const newTask = {
+            title: title,
+            description: description,
+            id: nextId,
+            priority: priority
+        };
+        this.toDoList.push(newTask);
+    },
+
+    deliteTask: function (id) {
+        const index = this.toDoList.findIndex(task => task.id === id);
+        if (index != -1) {
+            this.toDoList.splice(index, 1);
+        } else {
+            console.log('Задача не может быть выполнена');
+        }
+    },
+
+    updateTask: function (id, newTitle, newPriority, newDescription) {
+        const index = this.toDoList.findIndex(task => task.id === id);
+        if (index !== -1) {
+            if (newTitle !== undefined) {
+                this.toDoList[index].title = newTitle;
+            };
+            if (newPriority !== undefined) {
+                this.toDoList[index].priority = newPriority;
+            };
+            if (newDescription !== undefined) {
+                this.toDoList[index].description = newDescription;
+            };
+        } else {
+            console.log('Задача не может быть выполнена');
+        };
+    },
+
+    sortTasks: function (by, ascending) {
+        if (by === 'priority' || 'id' && ascending === 'true' || 'false') {
+            this.toDoList.sort((a, b) => {
+                if (ascending == true) {
+                    return a[by] - b[by];
+                } else {
+                    return b[by] - a[by];
+                };
+            }
+            );
+        } else {
+            console.log('Задача не может быть выполнена');
+        };
+    }
+};
+
+console.log(taskManager.toDoList);
